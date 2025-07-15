ec6de2f40fe78cb2a53b989afb6a93e4a5f59adb
omniladder <dustintobrien@gmail.com>
2025-07-14 23:40:12 -0400

    Actually working routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
 import { LoginScreen } from './login-screen/login-screen';
+import { CreateScreen } from './create-screen/create-screen';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LoginScreen
+    component: LoginScreen,
+    pathMatch: "full"
   },
   {
     path: 'login',
     component: LoginScreen
-  }
+  },
+  {
+    path: 'creation',
+    component: CreateScreen
+  },
 
 ];
--- src/app/create-screen/create-screen.ts
+++ src/app/create-screen/create-screen.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-create-screen',
-  imports: [],
+  selector: 'create-screen',
+  imports: [RouterOutlet],
   templateUrl: './create-screen.html',
   styleUrl: './create-screen.css'
 })
