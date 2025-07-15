4046c51274fc4a9e748cb935d85237e4911f0cd7
omniladder <dustintobrien@gmail.com>
2025-07-15 04:46:13 -0400

    Commiting current state

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginScreen } from './login-screen/login-screen';
 import { CreateScreen } from './create-screen/create-screen';
+import { HomeScreen } from './home-screen/home-screen';
+import { ChatScreen } from './chat-screen/chat-screen';
+import { NotFoundScreen } from './not-found-screen/not-found-screen';
 
 export const routes: Routes = [
   {
@@ -16,5 +19,16 @@ export const routes: Routes = [
     path: 'creation',
     component: CreateScreen
   },
-
+  {
+    path: 'home',
+    component: HomeScreen
+  },
+  {
+    path: 'chat',
+    component: ChatScreen
+  },
+  {
+    path: '**',
+    component: NotFoundScreen
+  }
 ];
---
+++ src/app/not-found-screen/not-found-screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-screen',
+  imports: [],
+  templateUrl: './not-found-screen.html',
+  styleUrl: './not-found-screen.css'
+})
+export class NotFoundScreen {
+
+}
